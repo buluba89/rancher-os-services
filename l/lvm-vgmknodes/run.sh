b86ccf924cce41acce4e969b0ad73541cff1445a
John Buluba <buluba89@gmail.com>
2020-05-16 18:37:42 +0300

    Update run.sh

--- l/lvm-vgmknodes/run.sh
+++ l/lvm-vgmknodes/run.sh
@@ -2,12 +2,9 @@
 
 echo Activating volume groups
 vgchange -a y &
-
-ls /dev
+sleep 4
 
 for i in $(vgs --reportformat json | jq -r '.report[].vg[].vg_name'); do
   echo Creating nodes for: $i  
   vgmknodes $i
 done
-
-ls /dev
